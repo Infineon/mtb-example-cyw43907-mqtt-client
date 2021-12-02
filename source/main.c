b6ab2e25c7d2931d3c667e632b4804bc0ae3e026
gitlab-runner <do-not-reply@cypress.com>
2021-12-02 05:31:25 -0600

    Upload mtb-example-cyw43907-mqtt-client 1.0.0.337

---
+++ source/main.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for MQTT Client Example for ModusToolbox.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* $ Copyright 2021 Cypress Semiconductor $
+*******************************************************************************/
+
+/* Header file includes */
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include "mqtt_task.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/******************************************************************************
+ * Function Name: main
+ ******************************************************************************
+ * Summary:
+ *  System entrance point. This function initializes retarget IO, sets up 
+ *  the MQTT client task, and then starts the RTOS scheduler.
+ *
+ * Parameters:
+ *  void
+ *
+ * Return:
+ *  int
+ *
+ ******************************************************************************/
+int main()
+{
+    cy_rslt_t result;
+
+    /* Initialize the board support package. */
+    result = cybsp_init();
+    CY_ASSERT(CY_RSLT_SUCCESS == result);
+
+    /* To avoid compiler warnings. */
+    (void) result;
+
+    /* Enable global interrupts. */
+    __enable_irq();
+
+    /* Initialize retarget-io to use the debug UART port. */
+    cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
+                        CY_RETARGET_IO_BAUDRATE);
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence to clear screen. */
+    printf("\x1b[2J\x1b[;H");
+    printf("===============================================================\n");
+    printf("CE234100 - CYW43907 MQTT Client\n");
+    printf("===============================================================\n\n");
+
+    /* Create the MQTT Client task. */
+    xTaskCreate(mqtt_client_task, "MQTT Client task", MQTT_CLIENT_TASK_STACK_SIZE,
+                NULL, MQTT_CLIENT_TASK_PRIORITY, NULL);
+
+    /* Start the FreeRTOS scheduler. */
+    vTaskStartScheduler();
+
+    /* Should never get here. */
+    CY_ASSERT(0);
+}
+
+/* [] END OF FILE */
